a567040327e6883a1e18025bee138e872f982c87
wccalvin <clayton@wccalvin.com>
2025-06-26 04:29:10 -0400

    first program

---
+++ concepts/01-hello/hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// single line comments
+
+/*
+multi-line
+comments
+*/
+
+// structure of the program with main
+int main() {
+  std::cout << "1... 2.. 3." << std::endl;
+  std::cout << "hello, world!\n";
+  return 0;
+}
