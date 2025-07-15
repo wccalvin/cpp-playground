971db522d75531725f84f2c2c7427fddf78e197b
wccalvin <clayton@wccalvin.com>
2025-07-14 21:11:56 -0400

    switch/case statements

---
+++ projects/magic_8.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main() {
+  std::cout << "MAGIC 8-BALL\n============\n";
+
+  // random number
+  srand(time(nullptr));
+  int answer = std::rand() % 10;
+  std::cout << "Answer (" << answer << "): ";
+
+  switch (answer) {
+    case 0:
+      std::cout << "It is certain.\n";
+      break;
+    case 1:
+      std::cout << "It is decidedly so.\n";
+      break;
+    case 2:
+      std::cout << "Without a doubt.\n";
+      break;
+    case 3:
+      std::cout << "Yes - definitely.\n";
+      break;
+    case 4:
+      std::cout << "Most likely.\n";
+      break;
+    case 5:
+      std::cout << "Concentrate and ask again.\n";
+      break;
+    case 6:
+      std::cout << "Better not tell you now.\n";
+      break;
+    case 7:
+      std::cout << "Reply hazy, try again.\n";
+      break;
+    case 8:
+      std::cout << "My sources say no.\n";
+      break;
+    default:
+      std::cout << "Very doubtful.\n";
+  }
+  std::cout << "------------\n";
+  return 0;
+}
