66ef34ecf21926e08a3945105ce2fc7aa12973f4
wccalvin <clayton@wccalvin.com>
2025-08-23 16:44:47 -0400

    booleans example

--- concepts/04-booleans.cpp
+++ concepts/04-booleans.cpp
@@ -21,5 +21,24 @@ int main() {
   bool isProcDead{!isProcAlive};
   cout << "isProcAlive is " << isProcAlive << endl;
   cout << "isProcDead is " << isProcDead << endl;
+
+  // common mistakes
+  // 1) need to repeat the variable name
+  int value = 10;
+  if (value == 5 || 20) {
+    cout << "how can this be true? ..." << endl;
+  }
+  if (value == 5 || value == 20) {
+    cout << "value is" << value << endl;
+  } else {
+    cout << "now this is fixed.." << endl;
+  }
+
+  // 2) && is evaluate first use () to clarify intentions
+  bool isReady{value == 10 || value == 20 && isProcDead};
+  cout << "isReady value is " << isReady << endl;
+  isReady = (value == 10 || value == 20) && isProcDead;
+  cout << "isReady value is now " << isReady << endl;
+
   return 0;
 }
