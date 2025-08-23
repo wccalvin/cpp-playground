10740fec969bb85f1cc5bbd143ae2044b709a478
wccalvin <clayton@wccalvin.com>
2025-08-23 10:16:51 -0400

    some more boolean example

--- concepts/04-booleans.cpp
+++ concepts/04-booleans.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 
 int main() {
+  // basic booleans
+  bool isAvailable{true};
+  cout << "isAvailable is " << isAvailable << endl;
+  isAvailable = false;
+  cout << "isAvailable is now " << isAvailable << endl;
+
+  // creating conditional boolean
   int level{10};
   bool isLevel9{level == 9};
   cout << "isLevel9 is " << isLevel9 << endl;
   level--;
   isLevel9 = (level == 9);
   cout << "isLevel9 is now " << isLevel9 << endl;
+
+  // inverting boolean
+  bool isProcAlive{true};
+  bool isProcDead{!isProcAlive};
+  cout << "isProcAlive is " << isProcAlive << endl;
+  cout << "isProcDead is " << isProcDead << endl;
   return 0;
 }
