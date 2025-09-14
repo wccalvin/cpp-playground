ea59619dff62fcd0a98849fb3fc789dc4f0b4a72
wccalvin <clayton@wccalvin.com>
2025-09-14 05:22:07 -0400

    function examples

--- projects/add.cpp
+++ projects/add.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
 int add(int a, int b) { return a + b; }
+bool isOdd(int n) { return n % 2; }
+void printBool(bool val) {
+  if (val) {
+    std::cout << "True";
+  } else {
+    std::cout << "False";
+  }
+}
 int main() {
   int value = add(5, 10);
   std::cout << "value is " << value << std::endl;
+  std::cout << "is 4 an odd number? ";
+  printBool(isOdd(4));
+  std::cout << std::endl;
+  std::cout << "is 7 an odd number? ";
+  printBool(isOdd(7));
+  std::cout << std::endl;
 }
